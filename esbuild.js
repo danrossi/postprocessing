3be3cecbf07559e83823b76bf7e4a839c060bf07
Raoul v. R <vanruesc@users.noreply.github.com>
2023-03-31 16:22:55 +0200

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,21 +1,12 @@
-import { createRequire } from "module";
-import { glsl } from "esbuild-plugin-glsl";
-import alias from "esbuild-plugin-alias";
+import pkg from "./package.json" assert { type: "json" };
 import esbuild from "esbuild";
+import { glsl } from "esbuild-plugin-glsl";
 import glob from "tiny-glob";
-import path from "path";
-import url from "url";
 
-//#region Values
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-const require = createRequire(import.meta.url);
-const pkg = require("./package");
 const minify = process.argv.includes("-m");
-const watch = process.argv.includes("-w");
 const external = ["three", "spatial-controls", "tweakpane"];
+const plugins = [glsl({ minify })];
 const date = new Date();
-
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date.toDateString()}
  * ${pkg.homepage}
@@ -23,65 +14,49 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
-const plugins = [
-	glsl({ minify }),
-	alias({
-		[pkg.name]: path.resolve(__dirname, "./src/index.ts"),
-		"temp": path.resolve(__dirname, "./temp")
-	})
-];
+// #region Library
 
-//#endregion
-
-//#region Library
-
-const ctxWorker = await esbuild.build({
-	entryPoints: await glob("src/**/worker.ts"),
+const workers = {
+	entryPoints: await glob("./src/**/worker.ts"),
 	outExtension: { ".js": ".txt" },
-	outdir: "temp",
+	outdir: "./temp",
 	target: "es6",
 	logLevel: "info",
 	format: "iife",
 	bundle: true,
 	minify
-});
+};
 
-await esbuild.build({
-	entryPoints: ["src/index.ts"],
-	outfile: `dist/${pkg.name}.js`,
+const lib = {
+	entryPoints: ["./src/index.ts"],
+	outfile: "./dist/index.js",
 	banner: { js: banner },
 	logLevel: "info",
 	format: "esm",
 	bundle: true,
 	external,
 	plugins
-});
+};
 
-if(watch) {
+// #endregion
 
-	await ctxWorker.watch();
+// #region Manual
 
-}
-
-//#endregion
-
-//#region Manual
-
-await esbuild.build({
-	entryPoints: ["manual/assets/js/libs/vendor.ts"],
-	outdir: "manual/assets/js/dist/libs",
+const vendor = {
+	entryPoints: ["./manual/assets/js/libs/vendor.ts"],
+	outdir: "./manual/assets/js/dist/libs",
 	globalName: "VENDOR",
 	target: "es6",
 	logLevel: "info",
 	format: "iife",
 	bundle: true,
 	minify
-});
+};
 
-const ctxManual = await esbuild.build({
-	entryPoints: ["manual/assets/js/src/index.ts"]
-		.concat(await glob("manual/assets/js/src/demos/*.ts")),
-	outdir: "manual/assets/js/dist",
+const manual = {
+	entryPoints: ["./manual/assets/js/src/index.ts"]
+		.concat(await glob("./manual/assets/js/src/demos/*.ts")),
+	outdir: "./manual/assets/js/dist",
 	logLevel: "info",
 	format: "iife",
 	target: "es6",
@@ -89,12 +64,24 @@ const ctxManual = await esbuild.build({
 	external,
 	plugins,
 	minify
-});
+};
 
-if(watch) {
+// #endregion
 
-	await ctxManual.watch();
+await esbuild.build(vendor);
+
+if(process.argv.includes("-w")) {
+
+	const ctxWorkers = await esbuild.context(workers);
+	await ctxWorkers.watch();
+	// const ctxManual = await esbuild.context(manual);
+	// await ctxManual.watch();
+
+} else {
+
+	await esbuild.build(workers);
+	await esbuild.build(lib);
+	// await esbuild.build(manual);
 
 }
 
-//#endregion
